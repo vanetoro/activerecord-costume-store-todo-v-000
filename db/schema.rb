075e5624430b34b8c4b488ff56d2cddd20a0b1ca
Vanessa Toro <vanetoro84@gmail.com>
2018-05-01 15:24:45 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "number_of_employees"
+    t.integer "num_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
