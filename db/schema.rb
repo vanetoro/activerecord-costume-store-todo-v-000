fdd3d551f7bc294c1dd111b87322054e84a09cfd
Vanessa Toro <vanetoro84@gmail.com>
2018-05-01 15:28:51 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.string "long_description"
+    t.string "description"
   end
 
 end
