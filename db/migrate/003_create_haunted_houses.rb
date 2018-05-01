adacfe512a421513b6fb9d16b6c9eb18c8037865
Vanessa Toro <vanetoro84@gmail.com>
2018-05-01 15:26:12 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
-      t.datetime :closing_time
+      t.datetime :closing_date
       t.string :long_description
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "theme"
     t.float "price"
     t.boolean "family_friendly"
-    t.datetime "opening_time"
+    t.datetime "opening_date"
     t.datetime "closing_time"
     t.string "long_description"
   end
